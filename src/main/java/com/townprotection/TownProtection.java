868fe706db0b17dec695dd89337e84f050a0f894
luke0630 <ryuto-0915@outlook.jp>
2024-05-27 19:40:34 +0900

    バージョン管理外ファイルの追加

---
+++ src/main/java/com/townprotection/TownProtection.java
@@ -0,0 +1,17 @@
+package com.townprotection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownProtection extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
