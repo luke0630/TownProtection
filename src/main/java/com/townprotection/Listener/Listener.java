e5b52384677305cb5737ac9ad66801af15d5cfe4
luke0630 <ryuto-0915@outlook.jp>
2024-07-04 12:57:21 +0900

    修正!!

--- src/main/java/com/townprotection/Listener/Listener.java
+++ src/main/java/com/townprotection/Listener/Listener.java
@@ -1,65 +1,31 @@
 package com.townprotection.Listener;
 
-import com.townprotection.Data.SelectorData.SelectorData;
 import com.townprotection.Selector.Selector;
 import com.townprotection.System.RunnableSystem;
 import com.townprotection.TownProtection;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.EquipmentSlot;
 
-import static com.townprotection.Data.MainData.*;
-import static com.townprotection.TownProtection.message;
-import static com.townprotection.Useful.getXYZMessage;
-import static com.townprotection.Useful.toColor;
+import static com.townprotection.Data.MainData.playerOpenGUI;
+import static com.townprotection.Data.MainData.setNameRunnable;
+import static com.townprotection.Range.ShowRange.PlaceFakeBlock;
+import static com.townprotection.Selector.Selector.changeSelectorDataPlayer;
 
 public class Listener implements org.bukkit.event.Listener {
     @EventHandler
     public void onClickBlock(PlayerInteractEvent event) {
         var player = event.getPlayer();
-
-        if (event.getHand() != EquipmentSlot.HAND) return; //二度呼ばれないようにする対策
-
-
-        if(Selector.IsSelectorTool(player)) {
-            player.sendMessage("あああんで");
-            if (event.getClickedBlock() == null) return;
-            if (!event.getClickedBlock().getType().isAir()) {
-                var location = event.getClickedBlock().getLocation();
-                var locData = playerSelectData.get(player);
-                if (locData == null) {
-                    locData = new SelectorData();
-                }
-
-                if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
-                    //左クリックで開始地点
-                    locData.startBlock = location;
-                    player.sendMessage(message + toColor("&d&l開始地点を設定しました。&f&l" + getXYZMessage(location)));
-                } else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    //左クリックで終了地点
-                    locData.endBlock = location;
-                    player.sendMessage(message + toColor("&d&l終了地点を設定しました。&f&l" + getXYZMessage(location)));
-                }
-
-                playerSelectData.put(player, locData);
-                if(locData.startBlock != null && locData.endBlock != null) {
-                    if(Selector.schedulers.containsKey(player)) {
-                        for(var schedule : Selector.schedulers.get(player)) {
-                            schedule.cancel();
-                        }
-                    }
-                    //パーティクルを表示する
-                    Selector.getRange(player, locData, Color.AQUA);
-                }
-                event.setCancelled(true);
-            }
+        if(!changeSelectorDataPlayer.containsKey(player)) return;
+        if(Selector.IsSelectorTool(player)  && event.getClickedBlock() != null) {
+            Bukkit.getScheduler().runTaskAsynchronously(TownProtection.instance, () -> {
+                PlaceFakeBlock(player, event.getClickedBlock().getLocation(), Material.GOLD_BLOCK);
+            });
         }
     }
 
