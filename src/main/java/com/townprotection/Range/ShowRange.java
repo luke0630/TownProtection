75a902dd096e47caeb2eb46c5124ec4e571c91eb
luke0630 <ryuto-0915@outlook.jp>
2024-07-04 12:55:41 +0900

    add: あああああ

--- src/main/java/com/townprotection/Range/ShowRange.java
+++ src/main/java/com/townprotection/Range/ShowRange.java
@@ -16,37 +16,103 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.townprotection.Selector.Selector.schedulers;
 import static com.townprotection.Selector.Selector.showRange;
 
 public class ShowRange {
     public static Map<Player, List<Block>> blocks = new HashMap<>();
-    public static void PlaceFakeBlock(Player player, Location location, Material material) {
-        var y = player.getWorld().getHighestBlockAt(location);
-        var clone = location.clone();
-        clone.setY(y.getY());
+        public static void PlaceFakeBlock(Player player, Location location, Material material) {
+        for(var loc : getHighBlock(location, 2)) {
+            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.BLOCK_CHANGE);
+            packet.getBlockPositionModifier().write(0, new BlockPosition(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+            packet.getBlockData().write(0, WrappedBlockData.createData(material.createBlockData()));
 
-        PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.BLOCK_CHANGE);
-        packet.getBlockPositionModifier().write(0, new BlockPosition(clone.getBlockX(), clone.getBlockY(), clone.getBlockZ()));
-        packet.getBlockData().write(0, WrappedBlockData.createData(material.createBlockData()));
+            var blockData = loc.getBlock();
+            if(blocks.containsKey(player)) {
+                blocks.get(player).add(blockData);
+            } else {
+                blocks.put(player, new ArrayList<>(List.of(blockData)));
+            }
 
-        var blockData = clone.getBlock();
-        if(blocks.containsKey(player)) {
-            blocks.get(player).add(blockData);
-        } else {
-            blocks.put(player, new ArrayList<>(List.of(blockData)));
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
         }
-
-        ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
     }
 
 
-    public static void ShowRangeWithBlock(Player player, SelectorData data, Material material) {
-        showRange(player, data, (Object object) -> {
-            if(object instanceof Location location) {
+    public static void setOnlyEdgeBlock(Player player, SelectorData data) {
+
+        var startX = data.startBlock.clone();
+        var startZ = data.startBlock.clone();
+        var endX = data.endBlock.clone();
+        var endZ = data.endBlock.clone();
+        var signumX = Math.signum(data.endBlock.getBlockX() - data.startBlock.getBlockX());
+        var signumZ = Math.signum(data.endBlock.getBlockZ() - data.startBlock.getBlockZ());
+        startX.setX(startX.getBlockX()+signumX);
+        startZ.setZ(startZ.getBlockZ()+signumZ);
+
+        PlaceFakeBlock(player, data.startBlock, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, startX, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, startZ, Material.GOLD_BLOCK);
+
+        startX.setX(startX.getBlockX()+signumX);
+        startZ.setZ(startZ.getBlockZ()+signumZ);
+
+        PlaceFakeBlock(player, startX, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, startZ, Material.GOLD_BLOCK);
+
+
+
+
+        endX.setX(endX.getBlockX()+(-1*signumX));
+        endZ.setZ(endZ.getBlockZ()+(-1*signumZ));
+
+        PlaceFakeBlock(player, data.endBlock, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, endX, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, endZ, Material.GOLD_BLOCK);
+
+
+        endX.setX(endX.getBlockX()+(-1*signumX));
+        endZ.setZ(endZ.getBlockZ()+(-1*signumZ));
+
+        PlaceFakeBlock(player, endX, Material.GOLD_BLOCK);
+        PlaceFakeBlock(player, endZ, Material.GOLD_BLOCK);
+    }
+    public static List<Location> getHighBlock(Location location, int count) {
+        int counter = 0;
+        List<Location> locationList = new ArrayList<>();
+        for(int i=320;i > -64;i--) {
+            var checkLoc = location.clone();
+            checkLoc.setY(i);
+            var checkBlock = checkLoc.getBlock();
+            if(checkBlock.getType().isCollidable()) {
+                if(counter < count) {
+                    counter++;
+                    locationList.add(checkBlock.getLocation());
+                } else {
+                    break;
+                }
+            }
+        }
+        if(!locationList.isEmpty()) {
+            return locationList;
+        }
+        return locationList;
+    }
+    public static void ShowRangeWithBlock(Player player, SelectorData data, Material material, boolean isRemove) {
+        if(isRemove) {
+            RemoveShowRange(player);
+        }
+        var blocks = showRange(player, data, null);
+        if(blocks.size() >= 250) {
+            setOnlyEdgeBlock(player, data);
+            return;
+        }
+        showRange(player, data, (Object s) -> {
+            if(s instanceof Location location) {
                 PlaceFakeBlock(player, location, material);
             }
         });
-    }
+     }
 
     public static void RemoveShowRange(Player player) {
         if(!blocks.containsKey(player)) return;
@@ -59,6 +125,15 @@ public class ShowRange {
             ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
 
         }
+        blocks.get(player).clear();
         blocks.remove(player);
+    }
+    public static void RemoveParticle(Player player) {
+        for(var scheduler : schedulers.values()) {
+            for(var sche : scheduler) {
+                sche.cancel();
+            }
+        }
+        schedulers.remove(player);
     }
 }
