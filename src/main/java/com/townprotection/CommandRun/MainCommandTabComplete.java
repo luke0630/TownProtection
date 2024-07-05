ef1f352195ba5b0173a43aba1dfd6d2a23ed0121
luke0630 <ryuto-0915@outlook.jp>
2024-07-05 15:00:14 +0900

    add: コマンドのタブ補完の追加

--- src/main/java/com/townprotection/CommandRun/MainCommandTabComplete.java
+++ src/main/java/com/townprotection/CommandRun/MainCommandTabComplete.java
@@ -1,17 +1,37 @@
 package com.townprotection.CommandRun;
 
+import com.townprotection.Data.MainData;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainCommandTabComplete implements TabCompleter {
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if(!command.getName().equalsIgnoreCase("townprotection")) return null;
+        if (args.length == 1) {
+            List<String> completions = Arrays.asList("wand", "open", "show", "deselect");
+            return completions.stream()
+                    .filter(option -> option.startsWith(args[0]))
+                    .collect(Collectors.toList());
+        } else if(args.length == 2) {
+            switch(args[0]) {
+                case "open" -> {
+                    List<String> towns = new ArrayList<>();
+                    for(var townData : MainData.townMarkData) {
+                        towns.add(townData.townName);
+                    }
+                    return towns;
+                }
+            }
+        }
         return null;
     }
 }
